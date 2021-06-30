bb985724510ddd2ed0571dc01225ead07bbd78be
letsdothispej <pauljamesazanes@gmail.com>
2021-06-30 23:28:05 +0800

    hamburger to close & collapse container padding

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
+const hamburgerMenu = document.querySelector(".hamburger-btn"),
+    hamburgerIcon = hamburgerMenu.querySelector(".hamburger-icon");
+
 const connectNav = document.getElementById("connectNav"),
-    connectBtn = connectNav.getElementById("connectBtn"),
-    connectCollapse = connectNav.querySelector(".collapse");
+    connectBtn = connectNav.querySelector("#connectBtn"),
+    connectCollapse = connectNav.querySelector("#connect");
 
 function init(){
-    connectBtn.addEventListener("click", handleClick);
-        
+    hamburgerMenu.addEventListener("click", handleClickMenu)
+    connectBtn.addEventListener("click", handleClickNav);
+    
 }
 
-function handleClick(event) {
-    console.log(event.target)
+function handleClickMenu(event){
+    updateCollapseAttribute(hamburgerMenu);
+    if(hamburgerMenu.getAttribute("collapsed") !== "true"){
+        hamburgerMenu.classList.add("opened");
+    } else {
+        if(hamburgerMenu.classList.contains("opened")){
+            hamburgerMenu.classList.remove("opened");
+        }
+    }
+}
+
+function handleClickNav(event) {
+    const btn = event.target;
+    updateCollapseAttribute(btn);
+}
+
+function updateCollapseAttribute(btn) {
+    if(btn.getAttribute("collapsed") !== "true" && hamburgerMenu.getAttribute("aria-expanded") !== "true") {
+        return btn.setAttribute("collapsed", "true");
+    } else {
+        return btn.setAttribute("collapsed", "false");
+    }
 }
 
 init();
