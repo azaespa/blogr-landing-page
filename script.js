483667a4ff0dd480dd0d34ae98ccdc77b32a5193
pjazanes <pauljamesazanes@gmail.com>
2021-06-30 15:02:41 +0800

    nav collapse style

---
+++ script.js
@@ -0,0 +1,16 @@
+const connectNav = document.getElementById("connectNav"),
+    connectBtn = connectNav.getElementById("connectBtn"),
+    connectCollapse = connectNav.querySelector(".collapse");
+
+function init(){
+    connectBtn.addEventListener("click", handleClick);
+    
+}
+
+function handleClick(event) {
+    console.log(event.target)
+}
+
+init();
+
+
